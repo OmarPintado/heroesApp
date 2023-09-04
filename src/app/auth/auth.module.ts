75b42431b3f2d0ca744e9b75b2a264d51aee5ffc
GaOmPS <omarpintado98@gmail.com>
2023-09-03 23:05:39 -0500

    create heroes layout page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from "./auth.routing.module";
+import {MaterialModule} from "../material/material.module";
+
 import {LayoutPageComponent} from './pages/layout-page/layout-page.component';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {RegisterPageComponent} from './pages/register-page/register-page.component';
@@ -15,6 +17,7 @@ import {RegisterPageComponent} from './pages/register-page/register-page.compone
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule {
