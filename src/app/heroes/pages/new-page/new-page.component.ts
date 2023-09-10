c4c5ec7aff710ba74bd22a944cb9aabf36972d87
GaOmPS <omarpintado98@gmail.com>
2023-09-09 19:36:14 -0500

    Add reactive form

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Publisher} from "../../interfaces/hero.interface";
+import {HeroesService} from "../../services/heroes.service";
 
 @Component({
   selector: 'app-new-page',
@@ -7,12 +10,28 @@ import { Component } from '@angular/core';
   ]
 })
 export class NewPageComponent {
+
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>(''),
+  });
+
   public publishers = [
-    {
-      id:'DC Comics', desc:'DC - Comics'
-    },{
-      id:'Marvel Comics', desc:'Marvel - Comics'
-    }
+    { id:'DC Comics', desc:'DC - Comics' },
+    { id:'Marvel Comics', desc:'Marvel - Comics'}
   ]
+
+  constructor(private heroesService:HeroesService) {}
+
+  onSubmit():void{
+    if(this.heroForm.invalid) return;
+
+    this.heroesService.addHero()
+  }
 
 }
