8ad8ce7f01e85157ecd6d04733027f71db0e5222
GaOmPS <omarpintado98@gmail.com>
2023-09-17 22:17:22 -0500

    Add login and logout feature

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginPageComponent {
+
+  constructor( private authService: AuthService,
+               private router: Router
+  ) { }
+  onLogin():void {
+    this.authService.login('omarpintado@gmail.com', '12334')
+      .subscribe( user => [
+        this.router.navigate(['/'])
+      ])
+  }
 
 }
